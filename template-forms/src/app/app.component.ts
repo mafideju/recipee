89aceb6bd57bb3b93e8579689af4f74eab34880c
Marcio Mafideju Rodrigues <mafideju@outlook.com>
2020-08-24 12:24:44 -0300

    feat(template-forms): basic template form without styling

--- template-forms/src/app/app.component.ts
+++ template-forms/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgModel, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'template-forms';
+export class AppComponent implements OnInit {
+  email: string;
+  
+  ngOnInit() {
+    console.log('NgModel',NgModel)
+    console.log('NgForm', NgForm)
+  }
+
+  onSubmit() {
+    console.log(this.email);
+  }
 }
